49ff0027e9a9c2ff1926e69a373936104087b10a
ChrisMori <61075123+ChrisMori@users.noreply.github.com>
2020-10-03 21:04:39 +0800

    Add files via upload

---
+++ bb.php
@@ -0,0 +1,134 @@
+<html>
+    <title>My Webb</title> 
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bb.css">
+
+<head>
+    <div class="box1">
+      <div class="box2"></div>
+        <div class="box3">
+        </div>
+    </div>
+        <div class="box4">
+            <div class="box5"></div>
+    </div>
+    <h1>Fire Torch</h1>
+      <center>
+      <div id="container">
+      <ul class="navi">
+          <li>
+              <a href="">
+                  <div class="link-box">
+                      <h2 class="link-title">HOME
+            </h2>
+                  </div>
+              </a>
+          </li>
+          <li>
+              <a href="">
+                  <div class="link-box">
+                      <h2 class="link-title">ABOUT
+            </h2>
+                  </div>
+              </a>
+          </li>
+          <li>
+              <a href="">
+                  <div class="link-box">
+                      <h2 class="link-title">GALLERY
+            </h2>
+                  </div>
+              </a>
+          </li>
+          <li>
+              <a href="">
+                  <div class="link-box">
+                      <h2 class="link-title">COMMENTS
+            </h2>
+                  </div>
+              </a>
+          </li>
+      </ul>
+  </div>
+    </div>
+</head>
+    </center>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    
+  <body>   
+    <div class="carousel">
+  <img src="http://dummyimage.com/280x200/56AD30/fff.png&text=1" />
+  <img src="http://dummyimage.com/280x200/1560f0/fff.png&text=2" />
+  <img src="http://dummyimage.com/280x200/C03229/fff.png&text=3" />
+  </div>
+
+   </body>
+   <br>
+   <br>
+   
+  <footer>
+    <svg viewBox="0 0 120 28">
+     <defs> 
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="1" result="blur" />
+          <feColorMatrix in="blur" mode="matrix" values="
+               1 0 0 0 0  
+               0 1 0 0 0  
+               0 0 1 0 0  
+               0 0 0 13 -9" result="goo" />
+          <xfeBlend in="SourceGraphic" in2="goo" />
+        </filter>
+         <path id="wave" d="M 0,10 C 30,10 30,15 60,15 90,15 90,10 120,10 150,10 150,15 180,15 210,15 210,10 240,10 v 28 h -240 z" />
+      </defs> 
+    
+       <use id="wave3" class="wave" xlink:href="#wave" x="0" y="-2" ></use> 
+       <use id="wave2" class="wave" xlink:href="#wave" x="0" y="0" ></use>
+     
+     
+      <g class="gooeff" filter="url(#goo)">
+      <circle class="drop drop1" cx="20" cy="2" r="8.8"  />
+      <circle class="drop drop2" cx="25" cy="2.5" r="7.5"  />
+      <circle class="drop drop3" cx="16" cy="2.8" r="9.2"  />
+      <circle class="drop drop4" cx="18" cy="2" r="8.8"  />
+      <circle class="drop drop5" cx="22" cy="2.5" r="7.5"  />
+      <circle class="drop drop6" cx="26" cy="2.8" r="9.2"  />
+      <circle class="drop drop1" cx="5" cy="4.4" r="8.8"  />
+      <circle class="drop drop2" cx="5" cy="4.1" r="7.5"  />
+      <circle class="drop drop3" cx="8" cy="3.8" r="9.2"  />
+      <circle class="drop drop4" cx="3" cy="4.4" r="8.8"  />
+      <circle class="drop drop5" cx="7" cy="4.1" r="7.5"  />
+      <circle class="drop drop6" cx="10" cy="4.3" r="9.2"  />
+      
+      <circle class="drop drop1" cx="1.2" cy="5.4" r="8.8"  />
+      <circle class="drop drop2" cx="5.2" cy="5.1" r="7.5"  />
+      <circle class="drop drop3" cx="10.2" cy="5.3" r="9.2"  />
+        <circle class="drop drop4" cx="3.2" cy="5.4" r="8.8"  />
+      <circle class="drop drop5" cx="14.2" cy="5.1" r="7.5"  />
+      <circle class="drop drop6" cx="17.2" cy="4.8" r="9.2"  />
+      <use id="wave1" class="wave" xlink:href="#wave" x="0" y="1" />
+     </g>  
+        <!-- g mask="url(#xxx)">
+        <path   id="wave1"  class="wave" d="M 0,10 C 30,10 30,15 60,15 90,15 90,10 120,10 150,10 150,15 180,15 210,15 210,10 240,10 v 28 h -240 z" />
+        </g>
+      </g -->
+    
+    </svg>
+    
+    <center>
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-instagram"></a>
+      </center>
+      <br>
+  <br>
+    </footer>
+  
+    
+</html>
